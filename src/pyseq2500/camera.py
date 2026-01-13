727143afd0381f70522dc820ca01f44cf424e934
Kunal Pandit <kpandit8387@gmail.com>
2026-01-12 22:21:22 -0500

    add camera

---
+++ src/pyseq2500/camera.py
@@ -0,0 +1,239 @@
+import logging
+from pyseq_core.base_instruments import BaseCamera
+from attrs import define, field
+from typing import Union, List
+from pathlib import Path
+import warnings
+
+LOGGER = logging.getLogger("PySeq")
+
+STATUS = {0: "error", 1: "busy", 2: "ready", 3: "stable", 4: "unstable"}
+
+
+@define
+class EmulatedTDICamera:
+    camera_id: int = field()
+    exposure: float = field(default=1.0)
+    gain: int = field(default=1.0)
+    status: int = field(default=3)
+    properties: dict = field(factory=dict)
+    sensor_mode: str = field(default="TDI")
+    number_image_buffers: int = field(default=0)
+    left_emission: str = field(init=False)
+    right_emission: str = field(init=False)
+
+    def saveImage(self, image_name, image_path):
+        LOGGER.debug("Saving {image_path}/c{self.cam.left_emssion}_{image_name}.tiff")
+        LOGGER.debug("Saving {image_path}/c{self.cam.right_emssion}_{image_name}.tiff")
+        return 1
+
+    def setPropertyValue(self, property, value):
+        self.properties[property] = value
+
+    def getPropertyValue(self, property):
+        if property in self.properties:
+            return self.properties[property]
+        else:
+            self.properties[property] = 1.0
+            return 1.0
+
+    def get_status(self):
+        return 3
+
+    def shutdown(self):
+        pass
+
+    def setTDI(self):
+        self.setPropertyValue("sensor_mode", 4)
+        self.setPropertyValue("sensor_mode_line_bundle_height", 128)
+        self.sensor_mode = "TDI"
+
+    def setAREA(self):
+        self.setPropertyValue("sensor_mode", 1)
+        self.setPropertyValue("sensor_mode_line_bundle_height", 128)
+        self.sensor_mode = "AREA"
+
+    def captureSetup(self):
+        pass
+
+    def stopAcquisition(self):
+        pass
+
+    def startAcquisition(self):
+        pass
+
+    def freeFrames(self):
+        self.number_image_buffers = 0
+
+    def allocFrame(self, n_frames):
+        self.number_image_buffers = n_frames
+
+    def getFrameCount(self) -> int:
+        return self.number_image_buffers
+
+    def getFocusStack(self) -> list:
+        return [
+            1,
+        ]
+
+
+@define(kw_only=True)
+class TDICameras(BaseCamera):
+    """Wrapper around 2 instances of pyseq_core.dcam.HammatsuCamera."""
+
+    name: str = field(default="Cameras")
+    _cams: dict = field(factory=dict)
+    _status: list = field(default=[None, None])
+    _exposure: list = field(default=[None, None])
+    _gain: list = field(default=[None, None])
+
+    @property
+    def cams(self) -> dict:
+        return self._cams
+
+    def __iter__(self):
+        for cam in self._cams.values():
+            yield cam
+
+    async def initialize(self):
+        """Initialize cameras to TDI mode."""
+        for cam in self:
+            cam.setTDI()
+            cam.captureSetup()
+
+        await self.status()
+        await self.get_exposure()
+        await self.get_gain()
+
+    async def configure(self):
+        """Connect to cameras and configure channel names."""
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings("error", category=UserWarning)
+            try:
+                from pyseq_core.dcam import HamamatsuCamera
+
+                dcam_found = True
+            except UserWarning:
+                dcam_found = False
+
+        for i in range(2):
+            if i not in self.cams:
+                if dcam_found:
+                    self.cams[i] = HamamatsuCamera(i)
+                else:
+                    self.cams[i] = EmulatedTDICamera(i)
+
+        for cam in self:
+            if self.config["save_nt"]:
+                cam.left_emission = self.config[cam.camera_id]["nucleotides"][0]
+                cam.right_emission = self.config[cam.camera_id]["nucleotides"][1]
+            else:
+                cam.left_emission = self.config[cam.camera_id]["left_emission"]
+                cam.right_emission = self.config[cam.camera_id]["right_emission"]
+
+    async def shutdown(self):
+        """Shutdown cameras"""
+        for cam in list(self.cams.keys()):
+            self.cams[cam].shutdown()
+            del self.cams[cam]
+
+    async def status(self) -> List[str]:
+        """Get status from cameras"""
+        for i, cam in enumerate(self):
+            self._status[i] = STATUS[cam.get_status()]
+
+        return self._status
+
+    async def save_image(self, image_name: str, image_path: Union[str, Path]) -> int:
+        """Save images to image_path/channel_image_name.tiff and return number of bytes saved."""
+        nbytes = 0
+        for cam in self:
+            nbytes += cam.saveImage(image_name, image_path)
+
+    async def set_exposure(self, time: float) -> float:
+        """Sets the exposure time (s) for the camera.
+
+        The definition for exposure changes between TDI and AREA mode:
+
+        AREA mode:
+        The camera alternates between exposure and readout according to the internal setting.
+        Because the CCD is a full-frame transfer type, the readout time is contained in the exposure time.
+        The exposure time can vary anywhere from 5.12 ms to 1004 ms in units of 20 μs.
+
+        TDI mode:
+        The exposure time is determined by the TDI line shift signal interval.
+        The minimum interval that you can set is 20 μs.
+
+        Args:
+            time (float): The desired exposure time in seconds.
+
+        Returns:
+            float: The set exposure time in seconds.
+        """
+        for i, cam in enumerate(self):
+            time = cam.setPropertyValue("exposure_time", time / 1000)
+            self._exposure[i] = time * 1000
+        return self._exposure
+
+    async def get_exposure(self) -> float:
+        """Retrieves the current exposure time (s) sfrom the camera.
+
+        Returns:
+            float: The current exposure time in seconds.
+        """
+        for i, cam in enumerate(self):
+            self._exposure[i] = cam.getPropertyValue("exposure_time") * 1000
+        return self._exposure
+
+    @property
+    def gain(self) -> List[int]:
+        """Retrieves the cached contrast enhancement gain from the camera."""
+        return self._gain
+
+    async def set_gain(self, gain: int) -> int:
+        """Sets the contrast enhancement gain (0 to 15) for the camera.
+
+        Args:
+            gain (int): The desired contrast enhancement gain
+        """
+        for i, cam in enumerate(self):
+            self._gain[i] = cam.setPropertyValue("contrast_gain", gain)
+        return gain
+
+    async def get_gain(self):
+        """Retrieves the current contrast enhancement gain from the camera.
+
+        Returns:
+            int: The current contrast enhancement gain.
+        """
+        for i, cam in enumerate(self):
+            self._gain[i] = cam.getPropertyValue("contrast_gain")
+        return self._gain
+
+    async def capture(self):
+        """Captures an image using the camera.
+
+        This is an abstract asynchronous method that must be implemented by
+        subclasses to define how the camera acquires an image. Captured images
+        are assumed to be stored in the camera's internal memory.
+
+        Raises:
+            NotImplementedError: If the method is not implemented by a subclass.
+        """
+        raise NotImplementedError
+
+    async def setTDI(self):
+        """Put cameras into TDI mode."""
+        for cam in self:
+            cam.setTDI()
+
+    async def setAREA(self):
+        """Put cameras into AREA mode."""
+        for cam in self:
+            cam.setAREA()
+
+    async def captureSetup(self):
+        """Configure cameras for capturing new images"""
+        for cam in self:
+            cam.captureSetup()
---
+++ tests/test_cameras.py
@@ -0,0 +1,41 @@
+from pyseq2500.camera import TDICameras
+import pytest
+import pytest_asyncio
+
+
+@pytest_asyncio.fixture(
+    params=[
+        pytest.param("MockCamera", marks=pytest.mark.mock),
+        pytest.param("Camera", marks=pytest.mark.hardware),
+    ],
+    scope="class",
+)
+async def cameras(request):
+    cameras = TDICameras()
+    await cameras.configure()
+
+    yield cameras
+
+    await cameras.shutdown()
+
+
+@pytest.mark.camera
+@pytest.mark.asyncio
+class TestCameras:
+    @pytest.mark.diagnostic
+    async def test_init(self, cameras: TDICameras):
+        await cameras.initialize()
+        for cam in cameras:
+            assert cam.sensor_mode == "TDI"
+        assert all(cameras._status)
+
+    @pytest.mark.diagnostic
+    async def test_area(self, cameras: TDICameras):
+        await cameras.setAREA()
+        for cam in cameras:
+            assert cam.sensor_mode == "AREA"
+        assert all(cameras._status)
+
+    async def test_shutdown(self, cameras: TDICameras):
+        await cameras.shutdown()
+        assert len(cameras.cams) == 0
